b714f08d9ae6b65b408f863757ef33f5883f4fc4
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-10-11 21:30:39 -0400

    added randomizer to ai file. ai should randomly choose a number and based on this number it will make a selection for the game

--- ai.py
+++ ai.py
@@ -1,2 +1,6 @@
 # use an integer randomizer that will allow the AI to choose an integer between 0-4.
 # each integer will represent one of the 5 options.
+
+import random
+print(random.randrange(0,5))
+
