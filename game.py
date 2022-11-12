0f6c3b40edceea4242a8135ec712c8e3c47a3ee0
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-11-11 22:10:53 -0500

    completed the code for each round, best of 3. updated all print statements

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game:
     def game_moves(self):
         while self.player_one.score < 2 and self.player_two.score < 2:
             if self.player_one.choice == self.player_two.choice:
-                print(f"Both users have chosen {self.player_one.choice}!\nTry again!")
+                print(f"Both players have chosen {self.player_one.choice}!\nTry again!")
             elif self.player_one.choice == "rock":
                 if self.player_two.choice == "scissor":
                     print(f"Rock smashes scissor!\nCongrats, {self.player_one.name}! You have won this round!")
@@ -37,28 +37,47 @@ class Game:
                     self.player_one.score += 1
                 else:
                     if self.player_two.choice == "scissor":
-                        print(f"{self.player_two.choice} cuts {self.player_one.choice}!\nThe AI has won this round!\nTry again!")
+                        print(f"Scissors cuts paper!\n{self.player_two.name} has won this round!\nTry again!")
                         self.player_two.score += 1
                     if self.player_two.choice == "lizard":
-                        print(f"{self.player_two.choice} eats {self.player_one.choice}!\nThe AI has won this round!\nTry again!")
-            elif self.choice == "scissor":
-                if self.ai_choice == "paper":
-                    print(f"Scissor cuts paper!")
-                    self.score += 1
-                if self.ai_choice == "lizard":
-                    print("Scissor decapitates lizard!")
-                    self.score += 1
-            elif self.choice == "lizard":
-                if self.ai_choice == "spock":
-                    print("Lizard poisons spock!")
-                    self.score += 1
-                if self.ai_choice == "paper":
-                    print("Lizard eats paper!")
-                    self.score += 1
-            elif self.choice == "spock":
-                if self.ai_choice == "scissor":
-                    print("Spock smahses scissor!")
-                    self.score += 1
-                if self.ai_choice == "rock":
-                    print("Spock vaporizes rock!")
-                    self.score += 1
+                        print(f"Lizard eats paper!\n{self.player_two.name} has won this round!\nTry again!")
+            elif self.player_one.choice == "scissor":
+                if self.player_two.choice == "paper":
+                    print(f"Scissor cuts paper!\nCongrats, {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                if self.player_two.choice == "lizard":
+                    print(f"Scissor decapitates lizard!\nCongrats, {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                else:
+                    if self.player_two.choice == "rock":
+                        print(f"Rock smashes scissors!\n{self.player_two.name} has won this round!\nTry again!")
+                        self.player_two.score += 1
+                    if self.player_two.choice == "spock":
+                        print(f"Spock smashes scissor!\n{self.player_two.name} has won this round!\nTry again!")
+            elif self.player_one.choice == "lizard":
+                if self.player_two.choice == "spock":
+                    print(f"Lizard poisons spock!\nCongrats, {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                if self.player_two.choice == "paper":
+                    print(f"Lizard eats paper!\nCongrats, {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                else:
+                    if self.player_two.choice == "rock":
+                        print(f"Rock smashes lizard!\n{self.player_two.name} has won this round!\nTry again!")
+                        self.player_two.score += 1
+                    if self.player_two.choice == "scissor":
+                        print(f"Scissor decapitates lizard!\n{self.player_two.name} wins this round!\nTry again!")
+                        self.player_two.score += 1
+            elif self.player_one.choice == "spock":
+                if self.player_two.choice == "scissor":
+                    print(f"Spock smashes scissor!\nCongrats {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                if self.player_two.choice == "rock":
+                    print(f"Spock vaporizes rock!\nCongrats, {self.player_one.name}! You have won this round!")
+                    self.player_one.score += 1
+                else:
+                    if self.player_two.choice == "lizard":
+                        print(f"Lizard poisons spock!\n{self.player_two.name} has won this round!\nTryagain!")
+                        self.player_two.score += 1
+                    if self.player_two.choice == "paper":
+                        print(f"Paper disproves spock!\n{self.player_two.name} wins this round!\nTry again!")
